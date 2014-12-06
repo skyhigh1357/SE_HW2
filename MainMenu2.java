97b165e081a204b42367d43aa26c87d4db1771b8
skyhigh1357 <skyhigh1357@naver.com>
2014-12-06 22:25:15 +0900

    modified login

--- MainMenu2.java
+++ MainMenu2.java
@@ -1,25 +1,80 @@
 package library;
 import java.io.*;
+import java.sql.*;
 
 
 public class MainMenu2 {
-	public void mainmenu2() throws IOException{ 
-		
+	
+	
+	MainMenu2() throws IOException{
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+			//System.exit(0);
+		}
+	}
+	
+	
+	public void mainmenu2() throws IOException, SQLException{ 
 		InputStreamReader sr=new InputStreamReader(System.in);
 		BufferedReader br=new BufferedReader(sr);
+		Connection conn = DriverManager.getConnection(staticvar.jdbcUrl, staticvar.userID, staticvar.userPass);
+		PreparedStatement ps= null;
+		String sql=null;
+		ResultSet rs = null;
 		
-		String id, pw;
-		
+		String id;
+		int pw;
+		while(true){
 		System.out.println("---------------------------");
 		System.out.println("로그인");
 		System.out.println("---------------------------");
 		System.out.print("아이디:	");
 		id=br.readLine();
 		System.out.print("비밀번호:	");
-		pw=br.readLine();
+		pw=Integer.parseInt(br.readLine());
 		
-		//디비 연동하기.
-		//아이디 비번 잘못 입력했을 경우, 잘못입력했다는 메시지 출력과 함께 다시 입력받기.
+		sql="select * from user where sid=?";
+		ps=conn.prepareStatement(sql);
+		ps.setString(1,  id);
+		rs=ps.executeQuery();
+		
+		
+		if(rs.next()){
+			String storedid = rs.getString("sid");
+			int storedpw = rs.getInt("pw");
+			
+			if(storedid.equals(id) && storedpw==pw){
+				staticvar.login(id);
+				System.out.println("로그인되었습니다.");
+				
+				if(staticvar.nowid.equals("admin")){
+					System.out.println("---------------------------");
+					System.out.println("admin님 환영합니다.");
+					System.out.println("---------------------------");
+					AdminMainMenu amm=new AdminMainMenu();
+					amm.adminmainmenu();
+					break;
+				}
+				else{
+					//학생계정처리
+					break;
+				}
+			}
+			else if(storedid==id){
+				System.out.println("비밀번호를 확인해주세요.");
+			}
+			else{
+				System.out.println("아이디와 비밀번호를 확인해주세요.");
+			}
+			
+		}
+		else{
+			System.out.println("아이디가 존재하지 않습니다.");
+		}
+		}
 		
 	}
+	
 }
